c4fa05a63a8d8a4efb404c003a3479ff4f37864b
Chaithanyasai Chowdary <chaithanyasaichowdary@Chaithanyasais-MacBook-Air.local>
2024-09-12 08:51:09 +0530

    waitress update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask,  render_template
+from flask import Flask, render_template
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def userDash():
    return render_template('userDash.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Serve the app using Waitress on port 8080
+    serve(app, host='0.0.0.0', port=8080)
